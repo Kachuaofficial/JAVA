845874f23942071b343af4aec64fa35e41ace8ff
kachuaofficial <shivammisra2021@gmail.com>
2026-02-22 15:30:40 +0530

    learned some concepts

---
+++ hashing/Tree_set.java
@@ -0,0 +1,14 @@
+package hashing;
+
+import java.util.TreeSet;
+
+public class Tree_set {
+    public static void main(String[] args) {
+
+        TreeSet<Integer> set  = new TreeSet<>();
+
+        set.add(88);
+
+        System.out.println(set);
+    }
+}
