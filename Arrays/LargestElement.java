e107868fc6fea2d91ab406d6d926878a5b4d4715
kachuaofficial <shivammisra2021@gmail.com>
2026-02-27 22:23:47 +0530

    some patterns

---
+++ Arrays/LargestElement.java
@@ -0,0 +1,18 @@
+
+public class LargestElement {
+
+    public static int Largest(int arr[]){
+
+        int Largest = arr[0];
+
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
